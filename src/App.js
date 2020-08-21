4b20dad7f406bd815f421cb29c8428087166a634
Carlos Lizarraga <carlos.lizarraga.felix@gmail.com>
2020-08-21 15:13:56 -0700

    commit friday

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import logo from './catt.jpg';
 import './App.css';
+import Moment from 'moment';
 
 function App() {
     return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Edit < code > src / App.js < /code> and save to reload. #NyanCatTeam.  <
-        /p> <
+        div className = "App">
+        <header className = "App-header">
+        <img src = { logo } className = "App-logo"
+        alt = "logo"/>
+        <p> Edit <code> src / App.js </code> and save to reload. #NyanCatTeam.</p> <
         a className = "App-link"
         href = "https://reactjs.org"
         target = "_blank"
         rel = "noopener noreferrer" >
-        Learn React <
-        /a> <
-        /header> <
-        /div>
+        Learn React </a> 
+        <p> Esto es un parrafo </p>
+        </header> 
+        </div>
     );
 }
+
+class Welcome extends React.Component {
+    render() {
+      return <h1>Hello, {this.props.name}</h1>;
+    }
+  }
 
 export default App;
