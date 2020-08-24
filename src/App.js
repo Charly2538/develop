4ce8f85f6d03d13050888d248ec49a5cb24ba7a6
Carlos Lizarraga <carlos.lizarraga.felix@gmail.com>
2020-08-24 10:42:06 -0700

    commit today

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import logo from './catt.jpg';
 import './App.css';
 import Moment from 'moment';
 
+
+
+
+
 function App() {
     return ( <
         div className = "App">
@@ -20,11 +24,5 @@ function App() {
         </div>
     );
 }
-
-class Welcome extends React.Component {
-    render() {
-      return <h1>Hello, {this.props.name}</h1>;
-    }
-  }
 
 export default App;
